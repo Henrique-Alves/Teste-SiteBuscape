e00dea51ff8c4d25a4285cfdea836727de9bc52b
Henrique-Alves <henriquehdsa@gmail.com>
2020-09-23 14:48:29 -0300

    Inicializando projeto

---
+++ src/test/java/br/hdsa/buscape/runners/Runner.java
@@ -0,0 +1,21 @@
+package br.hdsa.buscape.runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/Features/Teste_site_buscape.feature",
+		glue = "br.hdsa.buscape.steps",
+		tags = "",
+		plugin = {"pretty , html:target/report-html"},
+		monochrome = false,
+		snippets = SnippetType.CAMELCASE,
+		dryRun = false
+		)
+public class Runner {
+
+}
